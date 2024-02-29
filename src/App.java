ec63952bf28220d2aa68b3947c95b40e8a5c6e0d
hatsunemikael <angelinapham0533@gmail.com>
2024-02-29 15:33:56 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import processing.core.*;
+
+public class App extends PApplet{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        App app = new App();
+        app.runSketch();
+    }
+
+    public void settings(){
+        size(600,400);
+    }
+
+    public void setup(){
+        
+    }
+
+    public void draw(){
+        
+    }
+}
